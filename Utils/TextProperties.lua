a07c0eaf2578598aba2198859017196bc2fb590b
kori <77679412+Not-Kyle@users.noreply.github.com>
2026-01-15 11:54:20 -0800

    Create TextProperties.lua

---
+++ Utils/TextProperties.lua
@@ -0,0 +1,41 @@
+local TextProperties = {};
+TextProperties.__index = TextProperties;
+
+local function SetTextBounds(Self: Instance, XAxis: number, YAxis: number)
+    local SettingSize = UDim2.fromOffset(
+        math.max(XAxis, Self.TextBounds.X), 
+        math.max(YAxis, Self.TextBounds.Y)
+    )
+
+    Self.Size = SettingSize
+end
+
+
+local function SetAbsoluteSize(Self: Instance, Child: Instance, MinX: number, MinY: number, XAxis: number, YAxis: number)
+    local Size = UDim2.fromOffset(
+        math.max(MinX, Child.AbsoluteSize.X + (XAxis or 0)),
+        math.max(MinY, Child.AbsoluteSize.Y + (YAxis or 0))
+    )
+
+    Self.Size = Size;
+end
+
+
+function TextProperties:TextBounds(Self: Instance, XAxis: number, YAxis: number) : RBXScriptConnection
+    SetTextBounds(Self, XAxis, YAxis);
+    
+    return Self:GetPropertyChangedSignal('TextBounds'):Connect(function()
+        SetTextBounds(Self, XAxis, YAxis)
+    end)
+end
+
+
+function TextProperties:AbsoluteSize(Self: Instance, Child: Instance, MinX: number, MinY: number, XAxis: number, YAxis: number) : RBXScriptConnection
+    SetAbsoluteSize(Self, Child, MinX, MinY, XAxis, YAxis)
+
+    return Self:GetPropertyChangedSignal('AbsoluteSize'):Connect(function()
+        SetAbsoluteSize(Self, Child, MinX, MinY, XAxis, YAxis)
+    end)
+end
+
+return TextProperties;
