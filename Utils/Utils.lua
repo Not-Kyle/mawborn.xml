420322fc7732d179608a2cb71cd5a1a90deb0324
kori <77679412+Not-Kyle@users.noreply.github.com>
2026-01-18 01:50:49 -0800

    Update Utils.lua

--- Utils/Utils.lua
+++ Utils/Utils.lua
@@ -50,9 +50,9 @@ Utils.BothOriginal = Utils.Streets or Utils.Prison;
 Utils.BothPrisons = Utils.Prison or Utils.Remake;
 Utils.All = Utils.Streets or Utils.Prison or Utils.Remake;
 
-if Utils.Prison then
+-[[if Utils.Prison then
     game:shutdown(); -- I highly recommed not injecting Mawborn on prison, so you dont get logged for exploiting
-end
+end]]
 
 local Creators = {
     [5388525718]  = {Name = 'hellokittysouljia'},
