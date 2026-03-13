3618c8b7580ac3d2cda469c030132a219e767188
kori <77679412+Not-Kyle@users.noreply.github.com>
2026-03-13 15:23:50 -0700

    Update FileHandler.lua

--- Utils/FileHandler.lua
+++ Utils/FileHandler.lua
@@ -2,43 +2,36 @@ if getgenv().Mawborn.FileHandler then
     return
 end
 
-local Readfile, Writefile = readfile, writefile;
-local Isfolder, Makefolder = isfolder, makefolder;
-
-local Pcall = pcall;
-local Next = next;
-local Defer = task.defer;
+local Utils = Import('Utils/Utils.lua');
 
 local Folder = 'mawborn';
 local File = Folder .. '/source.xml';
 
-local Utils = Import('Utils/Utils.lua');
-
 local Config = {AutoExecute = true};
 
 function Config:UpdateFile()
-    return Writefile and Writefile(File, Utils.HttpService:JSONEncode(Config));
+    return writefile and writefile(File, Utils.HttpService:JSONEncode(Config));
 end
 
-Defer(function()
-    if not Makefolder
-        or not Isfolder
-        or not Readfile
-        or not Writefile then
+task.spawn(function()
+    if not makefolder
+        or not isfolder
+        or not readfile
+        or not writefile then
             
         return
     end
 
-    if Makefolder and not Isfolder(Folder) then 
-        Makefolder(Folder);
+    if makefolder and not isfolder(Folder) then 
+        makefolder(Folder);
     end
 
-    local Connection, Contents = Pcall(Readfile, File)
+    local Connection, Contents = pcall(readfile, File)
 
     if Connection then
         local Decode = Utils.HttpService:JSONDecode(Contents)
 
-        for Index, _ in Next, Config do
+        for Index, _ in next, Config do
             if Decode[Index] then
                 Config[Index] = Decode[Index]
             end
