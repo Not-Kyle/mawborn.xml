55baa8521b71db146300cc2918757334620a4185
kori <77679412+Not-Kyle@users.noreply.github.com>
2026-03-13 14:34:28 -0700

    Update Comands.lua

--- Utils/Comands.lua
+++ Utils/Comands.lua
@@ -2,6 +2,12 @@ if getgenv().Mawborn.Commands then
     return
 end
 
+local Typeof, Pcall = typeof, pcall;
+local Next = next;
+local Find, Remove = table.find, table.remove;
+local Split, Lower = string.split, string.lower;
+local OsClock = os.clock;
+
 local CommandCooldown = 0.5;
 local LastCommandTime = 0;
 
@@ -16,7 +22,7 @@ function CommandHandler.Add(...) -- Reworked version of Ciazware command handler
     local Arguments = { ... };
     local CommandNet = {};
 
-    if typeof(Arguments[1]) == 'table' then
+    if Typeof(Arguments[1]) == 'table' then
         CommandNet = Arguments[1];
     else
         CommandNet = {
@@ -33,23 +39,23 @@ function CommandHandler.Add(...) -- Reworked version of Ciazware command handler
 end
 
 function CommandHandler.Index(CommandName: string)
-    for _, Index in next, Commands do
-        if Index.Name == CommandName or (Index.Alias and table.find(Index.Alias, CommandName)) then
+    for _, Index in Next, Commands do
+        if Index.Name == CommandName or (Index.Alias and Find(Index.Alias, CommandName)) then
             return Index.Function
         end
     end
 end
 
 function CommandHandler.Execute(Arguments: string)
-    local Success, Error = pcall(function()
-        local Time = os.clock();
+    local Success, Error = Pcall(function()
+        local Time = OsClock();
 
         if Time - LastCommandTime < CommandCooldown then -- Stops double chat commands
             return
         end
 
-        local SplitArgs = string.split(Arguments, ' ')
-        local IndexedCommand = CommandHandler.Index(string.lower(table.remove(SplitArgs, 1)))
+        local SplitArgs = Split(Arguments, ' ')
+        local IndexedCommand = CommandHandler.Index(Lower(Remove(SplitArgs, 1)))
 
         if not IndexedCommand then
             return
