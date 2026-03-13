c64448b1e6f6a3b524e675e50a616780ff5d4ce1
kori <77679412+Not-Kyle@users.noreply.github.com>
2026-03-13 14:33:39 -0700

    Update TextProperties.lua

--- Utils/TextProperties.lua
+++ Utils/TextProperties.lua
@@ -2,13 +2,16 @@ if getgenv().Mawborn.TextProperties then
     return
 end
 
+local MathMax = math.max;
+local UD2fromOffset = UDim2.fromOffset;
+
 local TextProperties = {};
 TextProperties.__index = TextProperties;
 
 local function SetTextBounds(Self: Instance, XAxis: number, YAxis: number)
-    local SettingSize = UDim2.fromOffset(
-        math.max(XAxis, Self.TextBounds.X), 
-        math.max(YAxis, Self.TextBounds.Y)
+    local SettingSize = UD2fromOffset(
+        MathMax(XAxis, Self.TextBounds.X), 
+        MathMax(YAxis, Self.TextBounds.Y)
     )
 
     Self.Size = SettingSize
@@ -16,9 +19,9 @@ end
 
 
 local function SetAbsoluteSize(Self: Instance, Child: Instance, MinX: number, MinY: number, XAxis: number, YAxis: number)
-    local Size = UDim2.fromOffset(
-        math.max(MinX, Child.AbsoluteSize.X + (XAxis or 0)),
-        math.max(MinY, Child.AbsoluteSize.Y + (YAxis or 0))
+    local Size = UD2fromOffset(
+        MathMax(MinX, Child.AbsoluteSize.X + (XAxis or 0)),
+        MathMax(MinY, Child.AbsoluteSize.Y + (YAxis or 0))
     )
 
     Self.Size = Size;
@@ -27,19 +30,39 @@ end
 
 function TextProperties:TextBounds(Self: Instance, XAxis: number, YAxis: number) : RBXScriptConnection
     SetTextBounds(Self, XAxis, YAxis);
-    
-    return Self:GetPropertyChangedSignal('TextBounds'):Connect(function()
+
+    local Connection = Self:GetPropertyChangedSignal('TextBounds'):Connect(function()
         SetTextBounds(Self, XAxis, YAxis)
     end)
+
+    local KillConnection; KillConnection = Self.Destroying:Connect(function()
+        Connection:Disconnect();
+
+        if KillConnection then
+            KillConnection:Disconnect();
+        end
+    end)
+
+    return Connection;
 end
 
 
 function TextProperties:AbsoluteSize(Self: Instance, Child: Instance, MinX: number, MinY: number, XAxis: number, YAxis: number) : RBXScriptConnection
     SetAbsoluteSize(Self, Child, MinX, MinY, XAxis, YAxis)
 
-    return Self:GetPropertyChangedSignal('AbsoluteSize'):Connect(function()
+    local Connection = Self:GetPropertyChangedSignal('AbsoluteSize'):Connect(function()
         SetAbsoluteSize(Self, Child, MinX, MinY, XAxis, YAxis)
     end)
+
+    local KillConnection; KillConnection = Self.Destroying:Connect(function()
+        Connection:Disconnect();
+
+        if KillConnection then
+            KillConnection:Disconnect();
+        end
+    end)
+
+    return Connection;
 end
 
 return TextProperties;
