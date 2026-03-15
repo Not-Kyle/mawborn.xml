a57b668f72df90ca1d875aa3349d8c144063d00d
kori <77679412+Not-Kyle@users.noreply.github.com>
2026-03-15 03:33:04 -0700

    Update NewMenu.lua

--- UI/NewMenu.lua
+++ UI/NewMenu.lua
@@ -41,46 +41,6 @@ local Mouse = LocalPlayer:GetMouse();
 local Font = Enum.Font.Code
 local FontSize = 14;
 
-local ProxyContentProvider = game:GetService('ContentProvider');
-local ProxyCoreGui = game:GetService('CoreGui');
-
-local OldPreloading; OldPreloading = hookmetamethod(game, '__namecall', newcclosure(function(self, ...) -- Adding PreloadAsync bypass?
-    local Method = (getnamecallmethod or get_namecall_method)();
-    local Arguments = {...};
-
-    if not checkcaller() and (self == ProxyContentProvider or self == ContentProvider) then
-        if (Method == 'PreloadAsync' or Method == 'preloadAsync') then
-            local PreloadTable = Arguments[1];
-
-            if typeof(PreloadTable) == 'table' then
-                local ProxyTable = {};
-                local CoreGuiFound = false;
-
-                for _, Index in ipairs(PreloadTable) do
-                    if typeof(Index) == 'Instance' and ((Index == ProxyCoreGui or Index:IsDescendantOf(ProxyCoreGui)) or (Index == CoreGui or  Index:IsDescendantOf(CoreGui))) then
-                        CoreGuiFound = true;
-                    else
-                        table.insert(ProxyTable, Index);
-                    end
-                end
-
-                if CoreGuiFound then
-                    return OldPreloading(self, ProxyTable)
-                end
-            end
-        end
-
-        if (Method == 'GetAssetFetchStatus' or Method == 'getAssetFetchStatus') then
-            local Asset = Arguments[1];
-            if typeof(Asset) == 'string' and Asset:find('rbxassetid://') then
-                return Enum.AssetFetchStatus.None;
-            end
-        end
-    end
-
-    return OldPreloading(self, ...)
-end))
-
 local mawborn = Instance.new('ScreenGui'); -- I made two SGs? Whatever leave it idc
 if syn and syn.product_gui then
     syn.protect_gui(mawborn)
